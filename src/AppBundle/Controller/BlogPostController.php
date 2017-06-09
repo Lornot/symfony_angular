0f1ddee81e40c7f536f074a1d04493e0750668f4
Oleg Stakhiv <veishen19@gmail.com>
2017-06-09 14:13:21 +0000

    edit, delete and other staff

--- src/AppBundle/Controller/BlogPostController.php
+++ src/AppBundle/Controller/BlogPostController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\BlogPostType;
-
+use AppBundle\Entity\BlogPost;
 
 class BlogPostController extends Controller
 {
@@ -26,23 +26,55 @@ class BlogPostController extends Controller
     {
         $form = $this->createForm(BlogPostType::class);
 
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             $blogPost = $form->getData();
 
             $manager = $this->getDoctrine()->getManager();
-
             $manager->persist($blogPost);
             $manager->flush();
 
-            return $this->redirectToRoute('list');
+            return $this->redirectToRoute('edit', [
+                'blogPost' => $blogPost->getId()
+            ]);
         }
 
         return $this->render('BlogPosts/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    public function editAction(Request $request, BlogPost $blogPost)
+    {
+        $form = $this->createForm(BlogPostType::class, $blogPost);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+
+            return $this->redirectToRoute('edit', [
+                'blogPost' => $blogPost->getId()
+            ]);
+        }
+
+        return $this->render('BlogPosts/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function deleteAction(Request $request, BlogPost $blogPost)
+    {
+        if ($blogPost === null)
+            return $this->redirectToRoute('list');
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($blogPost);
+        $manager->flush();
+
+        return $this->redirectToRoute('list');
     }
 
 }
